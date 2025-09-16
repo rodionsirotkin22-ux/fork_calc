b361f6219a6aed898ad95ee85990c11d9cb0c9e1
Rodion <rodion.sirotkin.22@gmail.com>
2025-09-16 15:22:46 +0000

    test15

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css'; 
 import { ThemeProvider } from '@/components/theme-provider';
 
