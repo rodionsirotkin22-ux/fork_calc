c85e3881f8c5dc8424d30cd94b0d7a31ccaf505f
Rodion <rodion.sirotkin.22@gmail.com>
2025-09-16 14:50:58 +0000

    test14

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,16 @@
-import type { ReactNode } from "react";
-import "./global.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from '@vercel/analytics/next'; 
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export default function RootLayout({
   children,
-}: Readonly<{ children: ReactNode }>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-    <head>
-      <title>Кредитный калькулятор</title>
-      <meta name="description" content="Кредитный калькулятор с досрочными погашениями" />
-    </head>
+    <html lang="ru">
       <body>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-        {children}
-        </ThemeProvider>
-        </body>
+        <ThemeProvider>{children}</ThemeProvider>
+        <Analytics /> {/* Автоматически отправляет стандартные метрики */}
+      </body>
     </html>
   );
 }
-
