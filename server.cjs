d5ef864ba5c635cdefd7b64d5ea59d0c2feccffd
Rodion <rodion.sirotkin.22@gmail.com>
2025-09-19 12:06:54 +0000

    test12

--- server.cjs
+++ server.cjs
@@ -1,10 +1,12 @@
+# Импорт модулей и библиотек
 const express = require('express');
 const promBundle = require('express-prom-bundle');
 const path = require('path');
 
+# Создание экземпляра
 const app = express();
 
-// Middleware для сбора метрик
+# Middleware для сбора метрик
 const metricsMiddleware = promBundle({
   includeMethod: true,
   includePath: true,
@@ -13,17 +15,21 @@ const metricsMiddleware = promBundle({
   },
 });
 
+# Добавление в цепочку запросов
 app.use(metricsMiddleware);
 
-// Раздача статики
+# Раздача статических файлов из out через модуль
 app.use(express.static(path.join(__dirname, 'out')));
 
-// Fallback на index.html для SPA
+# Перехват get, возврат index, отправка клиенту
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'out', 'index.html'));
 });
 
+# Определяет порт прослушки
 const PORT = process.env.PORT || 3000;
+
+# Запуск express на порту и вывод сообщений
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
   console.log(`Метрики доступны по: http://localhost:${PORT}/metrics`);
